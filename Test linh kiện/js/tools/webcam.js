72f5c779d6bdc49fd0740cec34b2b7e82e87b201
hungnm3112 <43883760+hungnm3112@users.noreply.github.com>
2021-04-19 22:53:45 +0700

    Thêm chức năng check fps

--- Test linh kiện/js/tools/webcam.js
+++ Test linh kiện/js/tools/webcam.js
@@ -1,9 +1,11 @@
- function startup() {
+function startup() {
   var width = 320; // We will scale the photo width to this
   var height = 0; // This will be computed based on the input stream
   var streaming = false;
-  var video  = document.getElementById("video");
-
+  video = document.getElementById("video");
+  infoFPS = document.querySelector("div.info_FPS");
+  console.log(navigator)
+  
   navigator.mediaDevices
     .getUserMedia({ video: true, audio: false })
     .then(function (stream) {
@@ -22,9 +24,44 @@
         video.setAttribute("width", width);
         video.setAttribute("height", height);
         streaming = true;
-        document.getElementById("btnStart").setAttribute("style","display:none");
+        document
+          .getElementById("btnStart")
+          .setAttribute("style", "display:none");
+
+        videoF =  VideoFrame({
+            id: "video",
+            frameRate: 25,
+            callback: function (frame) {              
+              let currentTime = videoF.video.currentTime.toFixed(5);
+              let time = currentTime - lastTime
+              if(time>=1){
+                lastTime=currentTime;                
+                let fps = Math.round((frame-lastFrame)/time * 100) / 100
+                infoFPS.innerHTML = `${fps} FPS`;
+                lastFrame = frame;
+              }
+            },
+          });  
+        Change()  
       }
     },
     false
   );
+}
+
+// hàm lấy FPS
+var infoFPS = null;
+var videoF = null;
+var lastTime = 0;
+var lastFrame = 0;
+function Change() {
+  videoF.listen("frame");
+  lastTime = videoF.video.currentTime.toFixed(5);  
+  // if (videoF.video.paused) {
+  //   videoF.video.play();
+  //   videoF.listen("frame");
+  // } else {
+  //   videoF.video.pause();
+  //   videoF.stopListen();
+  // }
 }
