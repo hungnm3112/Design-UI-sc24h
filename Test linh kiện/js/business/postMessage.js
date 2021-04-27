a4401b043f2b39542c5af54ebbf3180d9f3b15c7
hungnm3112 <43883760+hungnm3112@users.noreply.github.com>
2021-04-27 11:31:30 +0700

    Tích hợp tools check webcam lên web sc

---
+++ Test linh kiện/js/business/postMessage.js
@@ -0,0 +1,11 @@
+window.parent.postMessage(
+  {
+    event_id: "tools_check",
+    data: {
+      location: "index",
+      height: "2515px",
+    },
+  },
+  "https://suachualaptop24h.com/ct-kiem-tra-webcam-online.html"
+);
+console.log("done post...")
