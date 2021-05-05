3fd07eff491e891c65c93179e6d8724ad4a8352f
hungnm3112 <43883760+hungnm3112@users.noreply.github.com>
2021-05-05 17:37:40 +0700

    Check bàn phím
    Hoàn thành bàn phím window

--- Test linh kiện/js/lib/DetectBrowser.js
+++ Test linh kiện/js/lib/DetectBrowser.js
@@ -34,3 +34,49 @@ function DetectBrowser() {
   }
   return detectBrowser;
 }
+
+function Browser() {
+  let browser = {
+    isChrome : false,
+    isFirefox : false,
+    isEdge : false,
+    isMSIE : false,
+    isSafari : false,
+    isOpera : false,
+    isYaBrowser : false,
+    isOther : false
+  };
+  // CHROME
+  if (navigator.userAgent.indexOf("Chrome") != -1) {
+    browser.isChrome = true;
+  }
+  // FIREFOX
+  else if (navigator.userAgent.indexOf("Firefox") != -1) {
+    browser.isFirefox = true;
+  }
+  // INTERNET EXPLORER
+  else if (navigator.userAgent.indexOf("MSIE") != -1) {
+    browser.isMSIE = true;
+  }
+  // EDGE
+  else if (navigator.userAgent.indexOf("Edge") != -1) {
+    browser.isEdge = true;
+  }
+  // SAFARI
+  else if (navigator.userAgent.indexOf("Safari") != -1) {
+    browser.isSafari = true;
+  }
+  // OPERA
+  else if (navigator.userAgent.indexOf("Opera") != -1) {
+    browser.isOpera = true;
+  }
+  // YANDEX BROWSER
+  else if (navigator.userAgent.indexOf("YaBrowser") != -1) {
+    browser.isYaBrowser = true;
+  }
+  // OTHERS
+  else {
+    browser.isOther = true;
+  }
+  return browser;
+}
