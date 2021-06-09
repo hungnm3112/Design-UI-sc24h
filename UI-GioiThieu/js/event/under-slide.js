f8b49d5ae1228e768f88ae0a6c903bf23472e83a
hungnm3112 <43883760+hungnm3112@users.noreply.github.com>
2021-06-09 10:57:48 +0700

    reset workspacw

--- UI-GioiThieu/js/event/under-slide.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded",()=>{
-    let liTags = Array.from(document.querySelectorAll(".carousel-indicators>li"))    
-    for(let li of liTags){                     
-        li.addEventListener("mouseover",(event)=>event.target.click())
-    }
-})
