dbf5e2832781fc068ed8920245bd33e94a5979a1
hungnm3112 <43883760+hungnm3112@users.noreply.github.com>
2021-07-07 18:41:42 +0700

    Đẩy tính năng test linh kiện lên web sc

---
+++ UI-Demo/UI-Tools-Check/js/business/postMessage-microphone.js
@@ -0,0 +1,10 @@
+window.parent.postMessage(
+  {
+    event_id: "tools_check",
+    data: {
+      location: "microphone",
+      height: "2000px",
+    },
+  },
+  "https://suachualaptop24h.com/ct-kiem-tra-webcam-online.html"
+);
---
+++ UI-Demo/UI-Tools-Check/js/business/postMessage-webcam.js
@@ -0,0 +1,16 @@
+let h = 0;
+if (isMobileLK) {
+  h = 395 
+} else {
+  h = 184
+}
+window.parent.postMessage(
+  {
+    event_id: "tools_check",
+    data: {
+      location: "index",
+      height: `${h}em`,
+    },
+  },
+  "https://suachualaptop24h.com/ct-kiem-tra-webcam-online.html"
+);
